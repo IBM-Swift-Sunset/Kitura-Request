4b16e18ccc7987db958095ac8be61714a620b091
Shmuel Kallner <kallner@il.ibm.com>
2017-01-09 10:40:20 +0200

    IBM-Swift/Kitura#955 This time fix the problem by merging files (#25)

--- Sources/KituraRequest/Encoding/MultipartFormData/BodyPart+MimeType.swift
+++ Sources/KituraRequest/Encoding/MultipartFormData/BodyPart+MimeType.swift
@@ -1,5 +1,6 @@
 /**
  * Copyright Sergey Minakov 2016
+ * Copyright IBM Corporation 2017
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -55,3 +56,80 @@ extension BodyPart {
         }
     }
 }
+
+
+extension BodyPart.MimeType {
+
+    /// Image media type.
+    public enum ImageSubtype: String {
+
+        /// Any image.
+        case any = "*"
+
+        /// A .gif image.
+        case gif = "gif"
+
+        /// A .jpeg image.
+        case jpeg = "jpeg"
+
+        /// A .png image.
+        case png = "png"
+
+        /// A .tiff image.
+        case tiff = "tiff"
+    }
+
+    /// Text media type.
+    public enum TextSubtype: String {
+
+        /// Any text.
+        case any = "*"
+
+        /// Text in .css format.
+        case css = "css"
+
+        /// Text in .csv format.
+        case csv = "csv"
+
+        /// Text in .html format.
+        case html = "html"
+
+        /// Text in javascript format.
+        case javascript = "javascript"
+
+        /// Plain text
+        case plain = "plain"
+
+        /// Text in .php format.
+        case php = "php"
+
+        /// Text in .xml format.
+        case xml = "xml"
+    }
+
+    /// Application media type.
+    public enum ApplicationSubtype: String {
+
+        /// Any binary data.
+        case any = "*"
+
+        /// Json data format.
+        case json = "json"
+
+        /// Javascript data format.
+        case javascript = "javascript"
+
+        /// Unrecognized binary data format.
+        case octetStream = "octet-stream"
+
+        /// A .pdf data format.
+        case pdf = "pdf"
+
+        /// A .zip data format.
+        case zip = "zip"
+
+        /// A .gzip data format.
+        case gzip = "gzip"
+    }
+}
+
